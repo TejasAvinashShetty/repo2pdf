eab56b36ecf19788a831e1b23860e1b1a0a6cf18
Tejas Shetty <tejasshetty.1808@gmail.com>
2019-10-19 22:28:34 +0530

    Added theory of  making paths

---
+++ src/path_maker.py
@@ -0,0 +1,8 @@
+'''
+Basically folder names don't end in a file extension.
+So one can sort out folders from files.
+After git cloning the repository will be present locally.
+It will consist of files and folders in the first level.
+After this level one will have to recursively go through each folder,
+and copy the paths
+'''
