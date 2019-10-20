0cc6f7a44a057e93073ae4221a37edc33d3523d1
Tejas Shetty <tejasshetty.1808@gmail.com>
2019-10-20 11:16:07 +0530

    Moved theory from .py to .txt files of the same name This was done - to avoid flake8 from getting angry due to module imports not being   at the top of the file - to have better looking, informative git diffs

--- src/pdf_maker.py
+++ src/pdf_maker.py
@@ -1,9 +1,1 @@
-'''
-Once, the paths are made by path maker,
-- make a folder "repo_name_pdf"
-- Make a similar folder structure as in the repo
-- Use wkhtml2pdf to make the pdfs from each file
-- save the resulting  pdf file in the simlar way as it's original avatar
-"i.e. in the same relative path as the original code file but in the
- path begining from 'repo_name_pdf' instead of 'repo_name'"
-'''
+
